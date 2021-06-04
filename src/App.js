e15d1d8247e51f5b627d8d083700826dba8d035f
Guilherme G Silva <gui_g88@hotmail.com>
2021-06-04 15:18:37 -0300

    some more logic4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import Row from "./Row";
+import requests from "./requests";
+
 const App = () => {
     return (
         <div>
-            Hello!!!
+            <Row
+                title="NETFLIX ORIGINALS"
+                fetchUrl={requests.fetchNetflixOriginals}
+            />
+            <Row
+                title="Trending Now"
+                fetchUrl={requests.fetchTrending}
+            />
+            <Row
+                title="Documentaries"
+                fetchUrl={requests.fetchDocumentaries}
+            />
+            <Row
+                title="Horror Movies"
+                fetchUrl={requests.fetchHorrorMovies}
+            />
+            <Row
+                title="Romance Movies"
+                fetchUrl={requests.fetchRomanceMovies}
+            />
+            <Row
+                title="Top Rated"
+                fetchUrl={requests.fetchTopRated}
+            />
+            <Row
+                title="Action Movies"
+                fetchUrl={requests.fetchActionMovies}
+            />
+            <Row
+                title="Comedy Movies"
+                fetchUrl={requests.fetchComedyMovies}
+            />
         </div>
-    )
-}
+    );
+};
 
-export default App
+export default App;
 
