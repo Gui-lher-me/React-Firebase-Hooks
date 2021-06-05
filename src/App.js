54a264e3b07b9e8c0b5a9f41df91e36fd287013c
Guilherme G Silva <gui_g88@hotmail.com>
2021-06-05 14:45:25 -0300

    added initial files

--- src/App.js
+++ src/App.js
@@ -1,44 +1,9 @@
-import Row from "./Row";
-import requests from "./requests";
+
+
 
 const App = () => {
-    return (
-        <div>
-            <Row
-                title="NETFLIX ORIGINALS"
-                fetchUrl={requests.fetchNetflixOriginals}
-            />
-            <Row
-                title="Trending Now"
-                fetchUrl={requests.fetchTrending}
-            />
-            <Row
-                title="Documentaries"
-                fetchUrl={requests.fetchDocumentaries}
-            />
-            <Row
-                title="Horror Movies"
-                fetchUrl={requests.fetchHorrorMovies}
-            />
-            <Row
-                title="Romance Movies"
-                fetchUrl={requests.fetchRomanceMovies}
-            />
-            <Row
-                title="Top Rated"
-                fetchUrl={requests.fetchTopRated}
-            />
-            <Row
-                title="Action Movies"
-                fetchUrl={requests.fetchActionMovies}
-            />
-            <Row
-                title="Comedy Movies"
-                fetchUrl={requests.fetchComedyMovies}
-            />
-        </div>
-    );
-};
+    return <h1>Hello</h1>
+}
 
-export default App;
+export default App
 
